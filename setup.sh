d5660471f8ac29091ced6fbc4c1e9319de6b4e9f
João Barreira <jpiresbarreira@gmail.com>
2019-06-02 17:43:09 +0100

    Add machine setup script and gitignore

---
+++ setup.sh
@@ -0,0 +1,9 @@
+sudo yum -y update
+
+sudo yum -y install wget vim tmux dstat bzip2 libXcomposite libXcursor libXi libXtst libXrandr alsa-lib mesa-libEGL libXdamage mesa-libGL libXScrnSaver
+
+wget https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
+
+bash ~/Anaconda3-2019.03-Linux-x86_64.sh
+
+rm ~/Anaconda3-2019.03-Linux-x86_64.sh
